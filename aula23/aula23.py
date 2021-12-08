77333b2b42bec5c1c8942d5fadb8671ad28fa988
raquellecampos <raquellecampos12@gmail.com>
2021-12-08 07:35:00 -0400

    aula

--- aula23/aula23.py
+++ aula23/aula23.py
@@ -6,3 +6,44 @@ mim, max
 range
 """
 
+#         0    1    2    3    4
+lista = ['A', 'B', 'C', 'D', 'E']
+#    -    5    4    3    2    1
+lista[2] = 'Raquelle'
+
+print(lista[0:4])  # posso excluir o 0.
+print(lista[2])
+print(lista)
+print(lista[-1])  # último objeto da lista
+
+
+lista1 = [1,2,3]
+lista2 = [4,5,6]
+
+lista1.extend(lista2)  # lista1 extende a lista2
+lista2.append('b')   # insere um valor no final da lista
+lista2.insert(0, 'banana')  # insere um valor no local do índice indicado
+lista1.pop()  # remove o último elemento da lista
+del(lista2[0]) # deleta o objeto por fatiamento
+
+
+print(lista1)
+print(lista2)
+
+################################################
+
+#l2 = list(range(0,100,8))
+l2 = [0,1,2,3,4,5,6,7,8,9]
+for valor in l2:
+    print(valor)
+
+
+
+
+
+
+
+
+
+
+
